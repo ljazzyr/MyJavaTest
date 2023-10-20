ec82f068d5d5e18c6e12e7c21fbe69f052206d68
“Jasmine” <LynnJazzy@Lynns-MacBook-Air.local>
2023-10-20 23:30:53 +0100

    HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
         System.out.println("Welcome to Java World Jasmine");
         System.out.println("2023");
         System.out.println("Exam time is coming");
+        System.out.println("Seunsola");
+        System.out.println("Whatever");
     }
 }
